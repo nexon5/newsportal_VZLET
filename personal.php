eba145c57b2aa94ef660b03a99430b7382f4bfbf
Dmitry Lychagin <dm.lychahin@gmail.com>
2021-05-11 12:28:48 +0300

    personal

--- personal.php
+++ personal.php
@@ -42,21 +42,15 @@ echo '</pre>';
           <li><a href="party.php">Состав</a></li>
           <li><a href="collection.php">Выпуски</a></li>
           <li><a href="contact.php">Набор</a></li>
-          <li><a href="contact.php">Личный кабинет</a></li>
+          <li><a href="personal.php">Личный кабинет</a></li>
             <li id="222">Добро пожаловать, <?php  echo $user ?> </li>
 
       </ul>
       </div>
 
       <div class="content">
-        <h1><i>Добро пожаловать!</i></h1>
-          <p>     Хаёвец, тебя приветствует наш дружный коллектив газеты "Взлёт". Всю необходимую информацию можешь найти на нашем сайте.
-            <br> Если хочешь влиться в наш дружный коллектив, то заходи во вкладку "Набор" и отправляй письмо нам на почту. Мы ждём тебя!
-            <br><br>
-            <br> <i>А пока посмотри наше последнее видео:</i>
-          </p>
-        <center>  <iframe width="560" height="315"  src="https://www.youtube.com/embed/Uj80rb2CTsA" frameborder="0"
-           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></center>
+        <h1><i>Добро пожаловать в личный кабинет, <?php$user  ?>!!! </i></h1>
+
       </div>
 
 
